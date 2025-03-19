c80dc08d75d59477d7bfd44aa0033a16b6b36db5
Julius M <67582275+sydhfjggt@users.noreply.github.com>
2025-03-19 10:56:40 +0100

    2025-02-19

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+
+let layer = document.querySelector(".fade-layer");
+layer.addEventListener("click", showMenu)
+
+let button = document.querySelector(".menu-button");
+button.addEventListener("click", showMenu)
+
+function showMenu(){
+    console.log("hhehehr")
+    let menu =  document.querySelector("nav.menu");
+    menu.classList.toggle("show");
+
+    let layer = document.querySelector(".fade-layer");
+    layer.classList.toggle("visible")
+}
